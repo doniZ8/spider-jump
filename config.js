34027128c6146950d7e2c013f19e083f8a3d968d
doniZ8 <deplaisir8@gmail.com>
2026-09-16 16:49:49 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.SPIDER_CONFIG = {
+  SUPABASE_URL: "https://qxtnjsfkloiugqoqkzrl.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4dG5qc2ZrbG9pdWdxb3FrenJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk1NTc0OTUsImV4cCI6MjEwNTEzMzQ5NX0.kk-2V47ZtYNsjMxEMr7q6afmpFaleAGh_b8e68qVk-Y"
+};
